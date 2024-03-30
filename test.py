d24afcba985184553d77b5e77d6ea6c6a5fa1d2f
fatima <i202658@nu.edu.pk>
2024-03-30 20:20:37 +0500

    Initial commit

--- test.py
+++ test.py
@@ -8,20 +8,21 @@ class TestApp(unittest.TestCase):
 
     def test_predict_endpoint(self):
         input_data = {
-            'alcohol': 13.24,
-            'malic_acid': 2.59,
-            'ash': 2.87,
-            'alcalinity_of_ash': 21.0,
-            'magnesium': 118.0,
-            'total_phenols': 2.8,
-            'flavanoids': 2.69,
-            'nonflavanoid_phenols': 0.39,
-            'proanthocyanins': 1.82,
-            'color_intensity': 4.32,
-            'hue': 1.04,
-            'od280/od315_of_diluted_wines': 2.93,
-            'proline': 735.0
+            'feature1': 7.8,
+            'feature2': 0.88,
+            'feature3': 0.96,
+            'feature4': 0.02,
+            'feature5': 1.8,
+            'feature6': 86.0,
+            'feature7': 3.45,
+            'feature8': 1.25,
+            'feature9': 0.5,
+            'feature10': 5.5,
+            'feature11': 1.1,
+            'feature12': 3.0,
+            'feature13': 1050.0
         }
+
         response = self.app.post('/predict', json=input_data)
         data = response.get_json()
 
